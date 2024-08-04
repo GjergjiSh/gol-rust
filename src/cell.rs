a75b2578875acb933a19b0714c69a4e13289201c
GjergjiSh <gjergji_shkurti@yahoo.com>
2024-08-04 13:54:34 +0200

    bugs in iterations, about to reogranize

--- src/cell.rs
+++ src/cell.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::{fmt, ptr};
 
 // Wrapper around a u8.
 // Represents the state of a cell.
@@ -10,11 +10,6 @@ use std::fmt;
 //  [x, x, x, |1, 0, 0, 0, |0] -> Dead cell with 8 neighbors
 #[derive(Debug, Copy, Clone)]
 pub struct Cell(u8);
-
-// Stack allocated 2D array of Cells
-// Neighbour counting algorithm is implemented here
-#[derive(Debug)]
-pub struct CellArray<const H: usize, const W: usize>([[Cell; W]; H]);
 
 impl Cell {
     fn new() -> Cell {
@@ -48,6 +43,25 @@ impl Cell {
     pub fn clear(&mut self) {
         self.0 = 0;
     }
+
+    //TODO: Weird behavior
+    pub fn increment_neighbour_count(&mut self) {
+        let count = (self.0 >> 1) & 0b1111;
+        assert!(count <= 8, "Neighbor count must be between 0 and 8");
+        // if count == 8 {
+        //     return;
+        // }
+        self.0 = (self.0 & 0b0000_0001) | ((count + 1) << 1);
+    }
+
+    pub fn decrement_neighbour_count(&mut self) {
+        let count = (self.0 >> 1) & 0b1111;
+        if count == 0 {
+            return;
+        }
+        // assert!(count >= 0, "Neighbor count must be between 0 and 8");
+        self.0 = (self.0 & 0b0000_0001) | ((count - 1) << 1);
+    }
 }
 
 impl PartialEq<u8> for Cell {
@@ -61,6 +75,12 @@ impl fmt::Display for Cell {
         write!(f, "{:08b}", self.0)
     }
 }
+
+//TODO: Copy/Clone vs memcopy
+// Stack allocated 2D array of Cells
+// Neighbour counting algorithm is implemented here
+#[derive(Debug, Copy, Clone)]
+pub struct CellArray<const H: usize, const W: usize>([[Cell; W]; H]);
 
 impl<const H: usize, const W: usize> CellArray<H, W> {
     pub fn new() -> CellArray<H, W> {
@@ -97,12 +117,84 @@ impl<const H: usize, const W: usize> CellArray<H, W> {
         self.0.iter_mut().flat_map(|row| row.iter_mut())
     }
 
-    pub fn rows(&self) -> usize {
+    pub fn width(&self) -> usize {
         H
     }
 
-    pub fn cols(&self) -> usize {
+    pub fn height(&self) -> usize {
         W
+    }
+
+    pub fn spawn_cell(&mut self, x: isize, y: isize) {
+        let neighbour_coordinates = self.neighbour_coordinates(x, y);
+
+        // let nghbr_cnt = self.calc_neighbours(x, y);
+        let cell = self.mut_cell(x, y);
+        cell.spawn();
+        // cell.set_neighbors(nghbr_cnt);
+
+        for (x, y) in neighbour_coordinates.iter() {
+            let cell = self.mut_cell(*x, *y);
+            cell.increment_neighbour_count();
+        }
+    }
+
+    pub fn kill_cell(&mut self, x: isize, y: isize) {
+        let neighbour_coordinates = self.neighbour_coordinates(x, y);
+
+        // let nghbr_cnt = self.calc_neighbours(x, y);
+        let cell = self.mut_cell(x, y);
+        cell.kill();
+        // cell.set_neighbors(nghbr_cnt);
+
+        for (x, y) in neighbour_coordinates.iter() {
+            let cell = self.mut_cell(*x, *y);
+            cell.decrement_neighbour_count();
+        }
+    }
+
+    pub fn neighbour_coordinates(&self, x: isize, y: isize) -> [(isize, isize); 8] {
+        [
+            (x.wrapping_sub(1), y.wrapping_sub(1)), // top_left
+            (x, y.wrapping_sub(1)),                 // top
+            (x.wrapping_add(1), y.wrapping_sub(1)), // top_right
+            (x.wrapping_sub(1), y),                 // left
+            (x.wrapping_add(1), y),                 // right
+            (x.wrapping_sub(1), y.wrapping_add(1)), // bottom_left
+            (x, y.wrapping_add(1)),                 // bottom
+            (x.wrapping_add(1), y.wrapping_add(1)), // bottom_right
+        ]
+    }
+
+    //TODO: Remove?
+    fn calc_neighbours(&self, x: isize, y: isize) -> u8 {
+        let mut neighbour_count = 0;
+
+        let neighbors = [
+            self.cell(x.wrapping_sub(1), y.wrapping_sub(1)), // top_left
+            self.cell(x, y.wrapping_sub(1)),                 // top
+            self.cell(x.wrapping_add(1), y.wrapping_sub(1)), // top_right
+            self.cell(x.wrapping_sub(1), y),                 // left
+            self.cell(x.wrapping_add(1), y),                 // right
+            self.cell(x.wrapping_sub(1), y.wrapping_add(1)), // bottom_left
+            self.cell(x, y.wrapping_add(1)),                 // bottom
+            self.cell(x.wrapping_add(1), y.wrapping_add(1)), // bottom_right
+        ];
+
+        for neighbor in neighbors.iter() {
+            if neighbor.is_alive() {
+                neighbour_count += 1;
+            }
+        }
+
+        neighbour_count
+    }
+
+    //TODO: This can be parallelized?
+    pub fn memcopy(&mut self, other: &mut Self) {
+        unsafe {
+            ptr::copy_nonoverlapping(self.0.as_ptr(), other.0.as_mut_ptr(), H * W);
+        }
     }
 }
 
@@ -120,8 +212,10 @@ impl<const H: usize, const W: usize> fmt::Display for CellArray<H, W> {
 
 #[cfg(test)]
 mod tests {
-    const ARRAY_H: usize = 2;
+    const ARRAY_H: usize = 5;
     const ARRAY_W: usize = 5;
+
+    use std::cell;
 
     use super::*;
 
@@ -184,7 +278,7 @@ mod tests {
     }
 
     #[test]
-    fn test_top_right_corner_to_bottom_left_corner() {
+    fn test_top_right_to_bottom_left_corner() {
         let mut cell_array = CellArray::<ARRAY_H, ARRAY_W>::new();
 
         // Should wrap to (ARRAY_W - 1, ARRAY_H - 1) which is the bottom-right cell
@@ -198,7 +292,7 @@ mod tests {
     }
 
     #[test]
-    fn test_bottom_left_corner_to_top_right_corner() {
+    fn test_bottom_left_to_top_right_corner() {
         let mut cell_array = CellArray::<ARRAY_H, ARRAY_W>::new();
 
         // Should wrap to (0, 0) which is the top-left cell
@@ -208,6 +302,58 @@ mod tests {
         cell.spawn();
 
         let destination = cell_array.cell(0, 0); // Top-left cell
+        assert_eq!(destination.is_alive(), true);
+    }
+
+    #[test]
+    fn test_top_left_to_bottom_left_corner() {
+        let mut cell_array = CellArray::<ARRAY_H, ARRAY_W>::new();
+
+        let x = 0;
+        let y = -1;
+        let cell = cell_array.mut_cell(x, y);
+        cell.spawn();
+
+        let destination = cell_array.cell(0, (ARRAY_H - 1) as isize); // Bottom-left cell
+        assert_eq!(destination.is_alive(), true);
+    }
+
+    #[test]
+    fn test_top_right_to_bottom_right() {
+        let mut cell_array = CellArray::<ARRAY_H, ARRAY_W>::new();
+
+        let x = ARRAY_W as isize - 1;
+        let y = -1;
+        let cell = cell_array.mut_cell(x, y);
+        cell.spawn();
+
+        let destination = cell_array.cell(ARRAY_W as isize - 1, ARRAY_H as isize - 1);
+        assert_eq!(destination.is_alive(), true);
+    }
+
+    #[test]
+    fn test_bottom_left_to_top_left() {
+        let mut cell_array = CellArray::<ARRAY_H, ARRAY_W>::new();
+
+        let x = 0;
+        let y = ARRAY_H as isize;
+        let cell = cell_array.mut_cell(x, y);
+        cell.spawn();
+
+        let destination = cell_array.cell(0, 0);
+        assert_eq!(destination.is_alive(), true);
+    }
+
+    #[test]
+    fn test_bottom_right_to_top_right() {
+        let mut cell_array = CellArray::<ARRAY_H, ARRAY_W>::new();
+
+        let x = ARRAY_W as isize - 1;
+        let y = ARRAY_H as isize;
+        let cell = cell_array.mut_cell(x, y);
+        cell.spawn();
+
+        let destination = cell_array.cell(ARRAY_W as isize - 1, 0);
         assert_eq!(destination.is_alive(), true);
     }
 
@@ -373,6 +519,30 @@ mod tests {
     }
 
     #[test]
+    fn test_increment_neighbours() {
+        let mut cell_array = setup();
+        let cell = cell_array.mut_cell(0, 0);
+        cell.increment_neighbour_count();
+        assert_eq!(cell.neighbour_cnt(), 1);
+        assert_eq!(cell.to_string(), "00000010");
+        assert_eq!(*cell == 0b00000010, true);
+    }
+
+    #[test]
+    fn test_decrement_neighbours() {
+        let mut cell_array = setup();
+        let cell = cell_array.mut_cell(0, 0);
+        cell.set_neighbors(1);
+        assert_eq!(cell.neighbour_cnt(), 1);
+        assert_eq!(cell.to_string(), "00000010");
+        assert_eq!(*cell == 0b00000010, true);
+        cell.decrement_neighbour_count();
+        assert_eq!(cell.neighbour_cnt(), 0);
+        assert_eq!(cell.to_string(), "00000000");
+        assert_eq!(*cell == 0b00000000, true);
+    }
+
+    #[test]
     fn test_reset() {
         let mut cell_array = setup();
         let cell = cell_array.mut_cell(0, 0);
@@ -387,4 +557,178 @@ mod tests {
         assert_eq!(cell.to_string(), "00000000");
         assert_eq!(*cell == 0b00000000, true);
     }
+
+    #[test]
+    fn test_glider() {
+        let mut cell_array = setup();
+
+        let x = 0;
+        let y = 0;
+        let pattern_coords = [
+            (x + 2, y),
+            (x + 2, y + 1),
+            (x + 2, y + 2),
+            (x + 1, y + 2),
+            (x, y + 1),
+        ];
+
+        for &(x, y) in &pattern_coords {
+            cell_array.spawn_cell(x, y);
+        }
+
+        //First column
+        let c1_neighbours = cell_array.calc_neighbours(0, 0);
+        let c1 = cell_array.mut_cell(0, 0);
+
+        assert_eq!(c1.is_alive(), false);
+        assert_eq!(c1_neighbours, 1);
+        assert_eq!(c1.to_string(), "00000010");
+        assert_eq!(*c1 == 0b00000010, true);
+
+        let c2_neighbours = cell_array.calc_neighbours(0, 1);
+        let c2 = cell_array.mut_cell(0, 1);
+
+        assert_eq!(c2.is_alive(), true);
+        assert_eq!(c2_neighbours, 1);
+        assert_eq!(c2.to_string(), "00000011");
+        assert_eq!(*c2 == 0b00000011, true);
+
+        let c3 = cell_array.cell(0, 2);
+        let c3_neighbours = cell_array.calc_neighbours(0, 2);
+        assert_eq!(c3.is_alive(), false);
+        assert_eq!(c3_neighbours, 2);
+
+        let c4 = cell_array.cell(0, 3);
+        let c4_neighbours = cell_array.calc_neighbours(0, 3);
+        assert_eq!(c4.is_alive(), false);
+        assert_eq!(c4_neighbours, 1);
+
+        let c5 = cell_array.cell(0, 4);
+        let c5_neighbours = cell_array.calc_neighbours(0, 4);
+        assert_eq!(c5.is_alive(), false);
+        assert_eq!(c5_neighbours, 0);
+
+        //Second column
+        let c6 = cell_array.cell(1, 0);
+        let c6_neighbours = cell_array.calc_neighbours(1, 0);
+        assert_eq!(c6.is_alive(), false);
+        assert_eq!(c6_neighbours, 3);
+
+        let c7 = cell_array.cell(1, 1);
+        let c7_neighbours = cell_array.calc_neighbours(1, 1);
+        assert_eq!(c7.is_alive(), false);
+        assert_eq!(c7_neighbours, 5);
+
+        let c8 = cell_array.cell(1, 2);
+        let c8_neighbours = cell_array.calc_neighbours(1, 2);
+        assert_eq!(c8.is_alive(), true);
+        assert_eq!(c8_neighbours, 3);
+
+        let c9 = cell_array.cell(1, 3);
+        let c9_neighbours = cell_array.calc_neighbours(1, 3);
+        assert_eq!(c9.is_alive(), false);
+        assert_eq!(c9_neighbours, 2);
+
+        let c10 = cell_array.cell(1, 4);
+        let c10_neighbours = cell_array.calc_neighbours(1, 4);
+        assert_eq!(c10.is_alive(), false);
+        assert_eq!(c10_neighbours, 1);
+
+        //Third column
+        let c11 = cell_array.cell(2, 0);
+        let c11_neighbours = cell_array.calc_neighbours(2, 0);
+        assert_eq!(c11.is_alive(), true);
+        assert_eq!(c11_neighbours, 1);
+
+        let c12 = cell_array.cell(2, 1);
+        let c12_neighbours = cell_array.calc_neighbours(2, 1);
+        assert_eq!(c12.is_alive(), true);
+        assert_eq!(c12_neighbours, 3);
+
+        let c13 = cell_array.cell(2, 2);
+        let c13_neighbours = cell_array.calc_neighbours(2, 2);
+        assert_eq!(c13.is_alive(), true);
+        assert_eq!(c13_neighbours, 2);
+
+        let c14 = cell_array.cell(2, 3);
+        let c14_neighbours = cell_array.calc_neighbours(2, 3);
+        assert_eq!(c14.is_alive(), false);
+        assert_eq!(c14_neighbours, 2);
+
+        let c15 = cell_array.cell(2, 4);
+        let c15_neighbours = cell_array.calc_neighbours(2, 4);
+        assert_eq!(c15.is_alive(), false);
+        assert_eq!(c15_neighbours, 1);
+
+        //Fourth column
+        let c16 = cell_array.cell(3, 0);
+        let c16_neighbours = cell_array.calc_neighbours(3, 0);
+        assert_eq!(c16.is_alive(), false);
+        assert_eq!(c16_neighbours, 2);
+
+        let c17 = cell_array.cell(3, 1);
+        let c17_neighbours = cell_array.calc_neighbours(3, 1);
+        assert_eq!(c17.is_alive(), false);
+        assert_eq!(c17_neighbours, 3);
+
+        let c18 = cell_array.cell(3, 2);
+        let c18_neighbours = cell_array.calc_neighbours(3, 2);
+        assert_eq!(c18.is_alive(), false);
+        assert_eq!(c18_neighbours, 2);
+
+        let c19 = cell_array.cell(3, 3);
+        let c19_neighbours = cell_array.calc_neighbours(3, 3);
+        assert_eq!(c19.is_alive(), false);
+        assert_eq!(c19_neighbours, 1);
+
+        let c20 = cell_array.cell(3, 4);
+
+        let c20_neighbours = cell_array.calc_neighbours(3, 4);
+        assert_eq!(c20.is_alive(), false);
+        assert_eq!(c20_neighbours, 1);
+
+        //Fifth column
+        let c21 = cell_array.cell(4, 0);
+        let c21_neighbours = cell_array.calc_neighbours(4, 0);
+        assert_eq!(c21.is_alive(), false);
+        assert_eq!(c21_neighbours, 1);
+
+        let c22 = cell_array.cell(4, 1);
+        let c22_neighbours = cell_array.calc_neighbours(4, 1);
+        assert_eq!(c22.is_alive(), false);
+        assert_eq!(c22_neighbours, 1);
+
+        let c23 = cell_array.cell(4, 2);
+        let c23_neighbours = cell_array.calc_neighbours(4, 2);
+        assert_eq!(c23.is_alive(), false);
+        assert_eq!(c23_neighbours, 1);
+
+        let c24 = cell_array.cell(4, 3);
+        let c24_neighbours = cell_array.calc_neighbours(4, 3);
+        assert_eq!(c24.is_alive(), false);
+        assert_eq!(c24_neighbours, 0);
+
+        let c25 = cell_array.cell(4, 4);
+        let c25_neighbours = cell_array.calc_neighbours(4, 4);
+        assert_eq!(c25.is_alive(), false);
+        assert_eq!(c25_neighbours, 0);
+    }
+
+    /* #[test]
+    fn test_memcopy() {
+        let mut cell_array = setup();
+        let mut other = setup();
+
+        let cell = cell_array.mut_cell(0, 0);
+        cell.spawn();
+        cell.set_neighbors(8);
+
+        cell_array.memcopy(&mut other);
+
+        let cell = other.cell(0, 0);
+        assert_eq!(cell.neighbour_cnt(), 8);
+        assert_eq!(cell.is_alive(), true);
+        assert_eq!(cell.to_string(), "00010001");
+        assert_eq!(*cell == 0b00010001, true);
+    } */
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@
 mod cell;
 
 use cell::CellArray;
+use minifb::{Key, Window, WindowOptions};
+
+const ARRAY_H: usize = 10;
+const ARRAY_W: usize = 10;
+const SCALE: usize = 10;
+const VERBOSE: bool = false;
 
 /*
     Underpopulation: A live cell with fewer than 2 live neighbors dies.
@@ -12,18 +18,53 @@ use cell::CellArray;
     Reproduction: A dead cell with exactly 3 live neighbors becomes a live cell.
 */
 
-fn count_neighbors(cell_array: &CellArray<3, 4>, x: isize, y: isize) -> u8 {
+fn spawn_glider(
+    cell_array: &mut CellArray<ARRAY_H, ARRAY_W>,
+    top_left_x: isize,
+    top_left_y: isize,
+) {
+    let glider_coords = [
+        (top_left_x + 1, top_left_y),     // (1, 0)
+        (top_left_x + 2, top_left_y + 1), // (2, 1)
+        (top_left_x, top_left_y + 2),     // (0, 2)
+        (top_left_x + 1, top_left_y + 2), // (1, 2)
+        (top_left_x + 2, top_left_y + 2), // (2, 2)
+    ];
+
+    for &(x, y) in &glider_coords {
+        let cell = cell_array.mut_cell(x, y);
+        cell.spawn();
+    }
+}
+
+fn spawn_custom_pattern(cell_array: &mut CellArray<ARRAY_H, ARRAY_W>, x: isize, y: isize) {
+    let pattern_coords = [
+        (x + 2, y),
+        (x + 2, y + 1),
+        (x + 2, y + 2),
+        (x + 1, y + 2),
+        (x, y + 1),
+    ];
+
+    for &(x, y) in &pattern_coords {
+        let neighbour_count = count_neighbours(cell_array, x, y);
+        let cell = cell_array.mut_cell(x, y);
+        cell.spawn();
+    }
+}
+
+fn count_neighbours(cell_array: &CellArray<ARRAY_H, ARRAY_W>, x: isize, y: isize) -> u8 {
     let mut neighbour_count = 0;
 
     let neighbors = [
-        cell_array.cell(x.wrapping_sub(1), y.wrapping_add(1)), // top_left
-        cell_array.cell(x as isize, y.wrapping_add(1)),                 // top
-        cell_array.cell(x.wrapping_add(1), y.wrapping_add(1)), // top_right
-        cell_array.cell(x.wrapping_sub(1), y as isize),                 // left
-        cell_array.cell(x.wrapping_add(1), y as isize),                 // right
-        cell_array.cell(x.wrapping_sub(1), y.wrapping_sub(1)), // bottom_left
-        cell_array.cell(x as isize, y.wrapping_sub(1)),                 // bottom
-        cell_array.cell(x.wrapping_add(1), y.wrapping_sub(1)), // bottom_right
+        cell_array.cell(x.wrapping_sub(1), y.wrapping_sub(1)), // top_left
+        cell_array.cell(x, y.wrapping_sub(1)),                 // top
+        cell_array.cell(x.wrapping_add(1), y.wrapping_sub(1)), // top_right
+        cell_array.cell(x.wrapping_sub(1), y),                 // left
+        cell_array.cell(x.wrapping_add(1), y),                 // right
+        cell_array.cell(x.wrapping_sub(1), y.wrapping_add(1)), // bottom_left
+        cell_array.cell(x, y.wrapping_add(1)),                 // bottom
+        cell_array.cell(x.wrapping_add(1), y.wrapping_add(1)), // bottom_right
     ];
 
     for neighbor in neighbors.iter() {
@@ -35,32 +76,165 @@ fn count_neighbors(cell_array: &CellArray<3, 4>, x: isize, y: isize) -> u8 {
     neighbour_count
 }
 
-fn solve(cell_array: &mut CellArray<3, 4>) {
-    let rows = cell_array.rows();
-    let cols = cell_array.cols();
+fn solve(
+    cell_array: &mut CellArray<ARRAY_H, ARRAY_W>,
+    cell_array_copy: &mut CellArray<ARRAY_H, ARRAY_W>,
+) {
+    let rows = cell_array.width();
+    let cols = cell_array.height();
 
-    for x in 0..rows {
-        for y in 0..cols {
-            let neighbour_count = count_neighbors(cell_array, x as isize, y as isize);
-            let cell = cell_array.mut_cell(x as isize, y as isize);
+    cell_array_copy.clone_from(&cell_array);
+
+    for x in 0..rows - 1 {
+        for y in 0..cols - 1 {
+            let cell = cell_array_copy.mut_cell(x as isize, y as isize);
             let is_alive = cell.is_alive();
-
-            cell.set_neighbors(neighbour_count);
+            let neighbour_count = cell.neighbour_cnt();
+            if VERBOSE {
+                println!(
+                    "Cell at ({}, {}) has {} neighbours and is {}",
+                    x, y, neighbour_count, if is_alive { "alive" } else { "dead" }
+                );
+            }
+            
 
             if is_alive {
                 if neighbour_count < 2 || neighbour_count > 3 {
-                    cell.kill();
+                    if VERBOSE {
+                        println!("Killing cell at ({}, {})", x, y);
+                    }
+                    cell_array.kill_cell(x as isize, y as isize);
                 }
             } else {
                 if neighbour_count == 3 {
-                    cell.spawn();
+                    if VERBOSE {
+                        println!("Spawning cell at ({}, {})", x, y);
+                    }
+                    cell_array.spawn_cell(x as isize, y as isize);
                 }
             }
         }
     }
 }
+
+fn render_field(cell_array: &CellArray<ARRAY_H, ARRAY_W>, buffer: &mut Vec<u32>) {
+    for y in 0..ARRAY_H {
+        for x in 0..ARRAY_W {
+            let cell = cell_array.cell(x as isize, y as isize);
+            let color = if cell.is_alive() { 0x000000 } else { 0xFFFFFF };
+            for dy in 0..SCALE {
+                for dx in 0..SCALE {
+                    buffer[(y * SCALE + dy) * ARRAY_W * SCALE + (x * SCALE + dx)] = color;
+                }
+            }
+        }
+    }
+}
+
+fn render_field_console(cell_array: &CellArray<ARRAY_H, ARRAY_W>) {
+    // Print the top border with column indices
+    print!("   "); // Space for row indices
+    println!();
+
+    // Print the top border of the grid with column numbers
+    print!("  +");
+    for x in 0..ARRAY_W {
+        print!("-{}-+", x);
+    }
+    println!();
+
+    // Print the field with side borders and row indices
+    for y in 0..ARRAY_H {
+        print!("{:2}|", y); // Row index
+        for x in 0..ARRAY_W {
+            let cell = cell_array.cell(x as isize, y as isize);
+            let symbol = if cell.is_alive() { '*' } else { ' ' };
+            print!(" {} |", symbol);
+        }
+        println!(); // End of the row with a side border
+
+        // Print the horizontal border between rows without column numbers
+        print!("  +");
+        for _ in 0..ARRAY_W {
+            print!("---+");
+        }
+        println!();
+    }
+}
+
 fn main() {
-    let mut cell_array = CellArray::<3, 4>::new();
-    solve(&mut cell_array);
-    println!("{}", cell_array);
+    let mut cell_array = CellArray::<ARRAY_H, ARRAY_W>::new();
+    spawn_custom_pattern(&mut cell_array, 3, 3);
+
+    for x in 0..cell_array.width() - 1 {
+        for y in 0..cell_array.height() - 1 {
+            let neighbour_count = count_neighbours(&cell_array, x as isize, y as isize);
+            let cell = cell_array.mut_cell(x as isize, y as isize);
+            cell.set_neighbors(neighbour_count);
+           /*  if cell.is_alive() {
+                println!(
+                    "Cell at ({}, {}) has {} neighbours and is alive",
+                    x, y, neighbour_count
+                );
+            } */
+        }
+    }
+
+    render_field_console(&cell_array);
+    // println!("{}", cell_array);
+    {
+        let c1 = cell_array.cell(3, 4);
+        let c1_cnt = count_neighbours(&cell_array, 3, 4);
+        assert_eq!(c1_cnt, 1);
+        assert_eq!(*c1, 0b00000011);
+        assert_eq!(c1.to_string(), "00000011");
+        let c2 = cell_array.cell(4, 5);
+        let c2_cnt = count_neighbours(&cell_array, 4, 5);
+        assert_eq!(c2_cnt, 3);
+        assert_eq!(*c2, 0b00000111);
+        assert_eq!(c2.to_string(), "00000111");
+        let c3 = cell_array.cell(5, 3);
+        let c3_cnt = count_neighbours(&cell_array, 5, 3);
+        assert_eq!(c3_cnt, 1);
+        assert_eq!(*c3, 0b00000011);
+        assert_eq!(c3.to_string(), "00000011");
+        let c4 = cell_array.cell(5, 4);
+        let c4_cnt = count_neighbours(&cell_array, 5, 4);
+        assert_eq!(c4_cnt, 3);
+        assert_eq!(*c4, 0b00000111);
+        assert_eq!(c4.to_string(), "00000111");
+        let c5 = cell_array.cell(5, 5);
+        let c5_cnt = count_neighbours(&cell_array, 5, 5);
+        assert_eq!(c5_cnt, 2);
+        assert_eq!(*c5, 0b00000101);
+    }
+
+    //Shallow copy
+    let mut temp = cell_array;
+
+    for x in 0..4 {
+        println!("Iteration {}", x);
+        solve(&mut cell_array, &mut temp);
+        render_field_console(&cell_array);
+        println!();
+        // println!("{}", cell_array);
+    }
+
+    // let mut window = Window::new(
+    //     "Game of Life",
+    //     ARRAY_W * SCALE,
+    //     ARRAY_H * SCALE,
+    //     WindowOptions::default(),
+    // )
+    // .unwrap_or_else(|e| {
+    //     panic!("{}", e);
+    // });
+
+    // let mut buffer: Vec<u32> = vec![0; ARRAY_W * ARRAY_H * SCALE * SCALE];
+
+    // while window.is_open() && !window.is_key_down(Key::Escape) {
+    //     solve(&mut cell_array);
+    //     render_field(&cell_array, &mut buffer);
+    //     window.update_with_buffer(&buffer, ARRAY_W * SCALE, ARRAY_H * SCALE).unwrap();
+    // }
 }
